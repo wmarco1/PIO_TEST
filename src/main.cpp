e2eaa89cbdfc2f908d77bc9758ad4b7f635fe6db
wmarco1 <wmarco1@nait.ca>
2018-10-28 13:36:31 -0600

    VSCode/PIO Test

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  Serial.println("Hello world\n");
+  delay(1000);
+  // put your main code here, to run repeatedly:
+} 
